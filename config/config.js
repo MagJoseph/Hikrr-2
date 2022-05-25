c1f046c3fc7f80cc2db25bcfe54fc773a7eb75f1
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-05-25 11:00:10 -0400

    test2

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    database: "DATABASE_URL",
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
